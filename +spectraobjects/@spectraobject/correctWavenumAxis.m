3a9dab4cf77f5cc577e92b9025e446cd53cd01e3
brycebjork <bryce.j.bjork@gmail.com>
2016-11-14 08:52:28 -0700

    calibration update

--- +spectraobjects/@spectraobject/correctWavenumAxis.m
+++ +spectraobjects/@spectraobject/correctWavenumAxis.m
@@ -5,6 +5,7 @@ function correctWavenumAxis( obj, varargin )
     % First find 12 ms data
     ind = find(obj.t >= 12000);
     ind = ind(1);
+	ind = 1;
 
     % Get the data
     fitx = obj.wavenum;
@@ -30,9 +31,15 @@ function correctWavenumAxis( obj, varargin )
     D2O_S = D2O_S(indcs);
     D2O_wavenum = D2O_wavenum(indcs);
     
+	% Load in the spectrum
+	s = load('H:\Spectrum Library\OD\OD_v0_PGo.mat');
+	OD_S = s.OD_v0_PGo.linestrength;
+	OD_wavenum = s.OD_v0_PGo.lineposition;
+	
     % Open the fitting figure
     fitobj = spectraobjects.VIPAxaxis(centerWavenum,vertScale,horizScale,fity);
-    fitobj.setSimulationSpectrum(D2O_wavenum,D2O_S);
+    %fitobj.setSimulationSpectrum(D2O_wavenum,D2O_S);
+	fitobj.setSimulationSpectrum(OD_wavenum,OD_S);
 
     % Fit the x axis
     %fitobj.autoSelectPeaks();
