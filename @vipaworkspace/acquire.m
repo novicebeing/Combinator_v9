bc74a3dd4a8a734255482f32624f798388965432
brycebjork <bryce.j.bjork@gmail.com>
2016-11-15 11:09:48 -0700

    fixed acquire error

--- @vipaworkspace/acquire.m
+++ @vipaworkspace/acquire.m
@@ -10,43 +10,50 @@ function acquire(obj)
 
     % Set acquire buttons
     obj.acquiretab.AcquireButton.Enabled = false;
+    obj.acquiretab.AcquireSpectrumButton.Enabled = false;
     obj.acquiretab.StopAcquireButton.Enabled = true;
     
-    n = 0;
-    lastAcquire = now;
-    while ~obj.stopAcquireBoolean
-        [images,time,acquireType,refImagesBoolean] = obj.acquireFunction();
-        
-        % Check the output
-        numImages = size(images,3);
-        if ~isvector(time) || ...
-                numel(time) ~= numImages || ...
-                ~islogical(refImagesBoolean) || ...
-                ~isvector(refImagesBoolean) || ...
-                numel(refImagesBoolean) ~= numImages || ...
-                ~strcmp(acquireType,'image')
-            errordlg('Acquire function output is poorly formatted','Acquire Error','modal');
-            break
-        end
-        
-        acqTimeDiff = now - lastAcquire;
+    try % to acquire an image
+    
+        n = 0;
         lastAcquire = now;
-        
-        % Send the image to the appropriate image object
-        if ~strcmp(obj.acquiretab.imageDestTextField.Text,'none')
-            imageobjid = {obj.acquiretab.imageDestTextField.Text};
-            if ~strcmp(imageobjid,'none')
-				obj.ImagesList.setImages(imageobjid,images,time,refImagesBoolean);
+        while ~obj.stopAcquireBoolean
+            [images,time,acquireType,refImagesBoolean] = obj.acquireFunction();
+
+            % Check the output
+            numImages = size(images,3);
+            if ~isvector(time) || ...
+                    numel(time) ~= numImages || ...
+                    ~islogical(refImagesBoolean) || ...
+                    ~isvector(refImagesBoolean) || ...
+                    numel(refImagesBoolean) ~= numImages || ...
+                    ~strcmp(acquireType,'image')
+                errordlg('Acquire function output is poorly formatted','Acquire Error','modal');
+                break
             end
+
+            acqTimeDiff = now - lastAcquire;
+            lastAcquire = now;
+
+            % Send the image to the appropriate image object
+            if ~strcmp(obj.acquiretab.imageDestTextField.Text,'none')
+                imageobjid = {obj.acquiretab.imageDestTextField.Text};
+                if ~strcmp(imageobjid,'none')
+                    obj.ImagesList.setImages(imageobjid,images,time,refImagesBoolean);
+                end
+            end
+
+            pause(0.1);
+    %         if ishandle(h)
+    %             h.Children(2).Children.String = sprintf('Acquired %i Spectra, %.1f Hz...',n,1/(acqTimeDiff*24*3600));
+    %         end
+            statusString = sprintf('Acquired %i Spectra, %.1f Hz...',n,1/(acqTimeDiff*24*3600));
+            obj.StatusBar.setText(statusString,[],'west');
+            n = n+1;
         end
-        
-        pause(0.1);
-%         if ishandle(h)
-%             h.Children(2).Children.String = sprintf('Acquired %i Spectra, %.1f Hz...',n,1/(acqTimeDiff*24*3600));
-%         end
-        statusString = sprintf('Acquired %i Spectra, %.1f Hz...',n,1/(acqTimeDiff*24*3600));
-        obj.StatusBar.setText(statusString,[],'west');
-        n = n+1;
+    
+    catch err
+        uiwait(errordlg('Could not acquire an image.','Acquire Error'));
     end
     
     % Reset the status
